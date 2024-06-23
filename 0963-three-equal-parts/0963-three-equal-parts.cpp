1188997dbf530cb9a38d9b87fb9361564c7f8bf8
Divyank Khajuria <132362862+DivyankR16@users.noreply.github.com>
2024-06-23 12:05:39 +0530

    Time: 28 ms (63.96%), Space: 41.3 MB (93.69%) - LeetHub

--- 0963-three-equal-parts/0963-three-equal-parts.cpp
+++ 0963-three-equal-parts/0963-three-equal-parts.cpp
@@ -1,49 +1,44 @@
 class Solution {
 public:
-    vector<int> threeEqualParts(vector<int>& arr) {
-        int countone=count(arr.begin(),arr.end(),1);
-        int n=arr.size();
-        if(countone%3)
-        {
+    vector<int> threeEqualParts(vector<int>& a) {
+       int n=a.size();
+       int cnt=0;
+       for(int i=0;i<n;i++){
+        if(a[i]==1)cnt++;
+       }
+       if(cnt%3){
+        return {-1,-1};
+       }
+       if(cnt==0){
+        return {0,n-1};
+       }
+       int cur=0;
+       cnt/=3;
+       int p1=-1;
+       int p2=-1;
+       int p3=-1;
+       for(int i=0;i<n;i++){
+        if(a[i]==1){
+            if(cur==0){
+                p1=i;
+            }
+            if(cur==cnt){
+                p2=i;
+            }
+            if(cur==2*cnt){
+                p3=i;
+            }
+            cur++;
+        }
+       }
+       while(p3<n-1){
+          p1++;
+          p2++;
+          p3++;
+          if(a[p1]!=a[p2] || a[p2]!=a[p3] || a[p1]!=a[p3]){
             return {-1,-1};
-        }
-        if(countone==0)
-        {
-            return {0,n-1};
-        }
-        int total=countone/3;
-        int p1=0,p2=0,p3=0;
-        int count=0;
-        
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]==1)
-            {
-                if(count==0)
-                {
-                    p1=i;
-                }
-                else if(count==total)
-                {
-                    p2=i;
-                }
-                else if(count==2*total)
-                {
-                    p3=i;
-                }
-                count+=1;
-            }
-        }
-        while(p3<n-1)
-        {
-            p1+=1;
-            p2+=1;
-            p3+=1;
-            if(arr[p1]!=arr[p2] || arr[p2]!=arr[p3] || arr[p1]!=arr[p3])
-            {
-                return {-1,-1};
-            }
-        }
-        return {p1,p2+1};
+          }
+       }
+       return {p1,p2+1};
     }
 };
