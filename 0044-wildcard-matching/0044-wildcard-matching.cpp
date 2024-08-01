538b139305982cddb82cef63e4d62550c6d22963
Divyank Khajuria <132362862+DivyankR16@users.noreply.github.com>
2024-08-01 13:34:04 +0530

    Time: 83 ms (10.41%), Space: 15.8 MB (62.65%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
-vector<vector<int>>memo;
-    bool solver(int i,int j,string &s,string &p){
-        if(j==p.size()){
-            return i==s.size();
-        }
-        if(i==s.size()){
-             if(p[j]=='*'){
-                return memo[i][j]=solver(i,j+1,s,p);
-             }
-             else{
-                return false;
-             }
-        }
-        if(memo[i][j]!=-1){
-            return memo[i][j];
-        }
-        bool ans=false;
-        if(p[j]=='?'){
-            ans|=solver(i+1,j+1,s,p);
-        }
-        else if(p[j]!='*'){
-            ans|=(p[j]==s[i] && solver(i+1,j+1,s,p));
-        }
-        else{
-            // for(int k=i;k<=s.size();k++){
-            //     ans|=solver(k,j+1,s,p);
-            // }
-            ans|=solver(i,j+1,s,p);
-            ans|=solver(i+1,j,s,p);
-        }
-        return memo[i][j]=ans;
-    }
     bool isMatch(string &s, string &p) {
-        memo.assign(s.size()+1,vector<int>(p.size()+1,-1));
-        return solver(0,0,s,p);
+        vector<vector<bool>>dp(s.size()+1,vector<bool>(p.size()+1,0));
+        dp[0][0]=1;
+        int n=s.size();
+        int m=p.size();
+        bool ck=1;
+        for(int i=1;i<=m;i++){
+            ck=ck && (p[i-1]=='*');
+            dp[0][i]=ck;
+        }
+       for(int j=1;j<=m;j++){
+        for(int i=1;i<=n;i++){
+            if(p[j-1]=='?' || p[j-1]==s[i-1]){
+                dp[i][j]=dp[i][j] || dp[i-1][j-1];
+            }
+            else{
+                if(p[j-1]=='*'){
+                dp[i][j]=dp[i][j] || dp[i-1][j];
+                dp[i][j]=dp[i][j] || dp[i][j-1];
+                }
+            }
+        }
+       }
+       return dp[n][m];
     }
 };
