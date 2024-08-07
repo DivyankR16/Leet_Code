30559789168dff356ec7eb544c61e6959e7984fc
Divyank Khajuria <132362862+DivyankR16@users.noreply.github.com>
2024-08-07 13:04:12 +0530

    Time: 318 ms (5.02%), Space: 324.1 MB (5%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
-    bool isPallindrome(string &s){
-        int n=s.size();
-        for(int i=0;i<=n/2;i++){
-           if(s[i]!=s[n-i-1]){
-               return false;
-           }
+string s;
+unordered_map<int,vector<vector<string>>>mp;
+bool ispal(int i,int j){
+    while(i<j){
+        if(s[i]!=s[j]){
+            return false;
         }
-        return true;
+        i++;
+        j--;
     }
-    void partition(vector<vector<string>>&ans,vector<string>&temp,string &s){
-        if(s.size()==0){
-           ans.push_back(temp);
-           return;
-        }
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            string taken=s.substr(0,i+1);
-            if(isPallindrome(taken)){
-                temp.push_back(taken);
-                string left=s.substr(i+1);
-                partition(ans,temp,left);
-                temp.pop_back();
-            }
-        }
+    return true;
+}
+vector<vector<string>>solver(int i){
+    if(i==s.size()){
+        return vector<vector<string>>();
     }
+    if(mp.count(i)){
+        return mp[i];
+    }
+    vector<vector<string>>ans;
+    for(int j=i;j<s.size();j++){
+        if(ispal(i,j)){
+        vector<vector<string>>res=solver(j+1);
+        string cur=s.substr(i,j-i+1);
+        for(auto &x:res){
+            x.insert(x.begin(),cur);
+        }
+        if(res.empty()){
+            res.push_back({cur});
+        }
+        for(auto v:res){
+            ans.push_back(v);
+        }
+
+        }
+        
+    }
+    return mp[i]=ans;
+}
     vector<vector<string>> partition(string s) {
-        vector<vector<string>>ans;
-        vector<string>temp;
-        partition(ans,temp,s);
-        return ans;
+        this->s=s;
+        return solver(0);
     }
 };
