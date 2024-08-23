ece0677d47515dbd752965b4382c454a27a5178a
Divyank Khajuria <132362862+DivyankR16@users.noreply.github.com>
2024-08-23 17:09:12 +0530

    Time: 86 ms (5.2%), Space: 38.1 MB (5.01%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& a, int k) {
-        int n = a.size();
-        map<int, int> mp;
-        int sum = 0;
-        int ans = 0;
-        
-        // Initialize map to handle the case where the subarray itself is divisible by k
-        mp[0] = 1;
-
-        for (int i = 0; i < n; i++) {
-            sum += a[i];
-            
-            // Normalize remainder to be in the range [0, k-1]
-            int rem = (sum % k + k) % k;
-
-            // Increment answer by the count of subarrays that had the same remainder
-            ans += mp[rem];
-            
-            // Increment the count of this remainder
-            mp[rem]++;
+        int n=a.size();
+        map<int,int>mp;
+        int sum=0;
+        int ans=0;
+        mp[0]=1;
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+            ans+=mp[sum%k];
+            if(sum!=0 && sum>0){
+                    ans+=mp[-(k-(sum%k))];
+            }
+            if(sum!=0 && sum<0){
+                    ans+=mp[k+(sum%k)];
+            }
+            mp[sum%k]++;
         }
-        
         return ans;
     }
 };
